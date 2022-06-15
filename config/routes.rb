2ae54086e404da0bd175f51f2e20dab40f4da3fc
Michael Dahl <michael@westwood.software>
2022-06-15 15:29:36 +0200

    Add dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'checkout/new'
+  resource :dashboard, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "checkout#new"
+  root 'dashboards#show'
 end
